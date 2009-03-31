3187b66ee77547c09199e1a34b108773b80054f7
Pino Toscano <pino@kde.org>
2009-03-31 20:19:48 +0000

    extract messages from .h files as well
    svn path=/trunk/playground/games/palapeli/; revision=947586

--- app/Messages.sh
+++ app/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` rc.cpp -o $podir/palapeli.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h` rc.cpp -o $podir/palapeli.pot
 rm -f rc.cpp
