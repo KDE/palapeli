c76419dcd545899c3db215fd7af87de9468d9301
Stefan Majewsky <majewsky@gmx.net>
2009-09-25 21:18:52 +0000

    Add new "palapeli" directory and import libpala from the old palapeli source.
    svn path=/trunk/playground/games/palapeli/; revision=1028102

--- app/Messages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` rc.cpp -o $podir/palapeli.pot
-rm -f rc.cpp
--- editor/Messages.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-$XGETTEXT storage/*.cpp *.cpp -o $podir/paladesign.pot 
