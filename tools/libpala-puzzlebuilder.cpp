61cdcb3665f940c600d804c6cf539c4d29794b57
Stefan Majewsky <majewsky@gmx.net>
2009-09-27 16:58:45 +0000

    Add thumbnails to puzzles.
    svn path=/trunk/playground/games/palapeli/; revision=1028578

--- tools/libpala-puzzlebuilder.cpp
+++ tools/libpala-puzzlebuilder.cpp
@@ -139,6 +139,13 @@ int main(int argc, char** argv)
 			return 1;
 		}
 	}
+	//write thumbnail into tempdir
+	const QImage thumbnail = image.scaled(QSize(128, 128), Qt::KeepAspectRatio);
+	if (!thumbnail.save(cachePath + QLatin1String("thumbnail.jpg")))
+	{
+		std::cerr << "Could not save thumbnail." << std::endl;
+		return 1;
+	}
 	//write piece offsets into target manifest
 	QMap<int, QPoint> pieceOffsets = job.pieceOffsets();
 	KConfig targetManifest(targetManifestPath, KConfig::SimpleConfig);
