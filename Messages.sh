a3b7f5d52dd5ba02cb2e77fa92e8ebd4a1202e2a
Laurent Montel <montel@kde.org>
2009-09-28 11:40:30 +0000

    Fix extract
    svn path=/trunk/playground/games/palapeli/; revision=1028874

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$EXTRACTRC `find . -name \*.ui -o -name \*.rc` >> rc.cpp
+$EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` rc.cpp -o $podir/palapeli.pot
 rm -f rc.cpp
