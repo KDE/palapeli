e8f346a207be9504d175b4264dbb11ad25cdc4e6
Stefan Majewsky <majewsky@gmx.net>
2010-05-23 21:40:13 +0000

    Fix inconsistency in user-visible strings.
    The "puzzle table" in this string is actually the "puzzle table area"
    (as used everywhere else, including the documentation). The puzzle table
    is the complete widget, while the puzzle table area is that part of it
    where pieces can be moved.
    
    CCMAIL: kde-i18n-doc@kde.org
    ..because this might be a violation of the soft message freeze.
    
    svn path=/trunk/KDE/kdegames/palapeli/; revision=1129828

--- src/engine/constraintinteractor.cpp
+++ src/engine/constraintinteractor.cpp
@@ -24,7 +24,7 @@
 Palapeli::ConstraintInteractor::ConstraintInteractor(QGraphicsView* view)
 	: Palapeli::Interactor(10, Palapeli::MouseInteractor, view) //priority: less than interaction with pieces, but more than interaction with viewport
 {
-	setMetadata(TableInteraction, i18n("Change size of puzzle table by dragging its edges"), QIcon());
+	setMetadata(TableInteraction, i18n("Change size of puzzle table area by dragging its edges"), QIcon());
 }
 
 QList<Palapeli::ConstraintInteractor::Side> Palapeli::ConstraintInteractor::touchingSides(const QPointF& scenePos) const
