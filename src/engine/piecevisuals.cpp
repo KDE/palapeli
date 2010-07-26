0c5944a479d47587b797d9c98f52c2ef871f9e4d
Johannes Loehnert <loehnert.kde@gmx.de>
2010-07-26 18:54:26 +0000

    outline rendering for improved groove effect (+bling)
    svn path=/trunk/KDE/kdegames/palapeli/; revision=1155027

--- src/engine/piecevisuals.cpp
+++ src/engine/piecevisuals.cpp
@@ -115,6 +115,9 @@ static QPixmap createShadow(const QPixmap& source, int radius)
 Palapeli::BevelMap Palapeli::calculateBevelMap(const QPixmap& source, int radius)
 {
 	const qreal strength_scale = 0.2;
+	// in multiples of radius
+	const qreal outline_width = 0.08;
+	const qreal outline_darken_scale = 3;
 
 	QImage sourceimg = source.toImage();
 	// we only really care about the alpha channel here. And we expect ARGB32_Premultiplied anyway.
@@ -127,29 +130,44 @@ Palapeli::BevelMap Palapeli::calculateBevelMap(const QPixmap& source, int radius
 	int width = img.width();
 	int height = img.height();
 
-	// prepare the folding kernel
-	qreal** kernel = new qreal*[radius];
+	// prepare the folding kernels
+	qreal** kernel_bevel = new qreal*[radius];
+	qreal** kernel_outline = new qreal*[radius];
 	for (int n_xf=0; n_xf<radius; n_xf++)
 	{
-		kernel[n_xf] = new qreal[radius];
+		kernel_bevel[n_xf] = new qreal[radius];
+		kernel_outline[n_xf] = new qreal[radius];
 		for (int n_yf=0; n_yf<radius; n_yf++)
 		{
 			qreal xfold = n_xf + 0.5;
 			qreal yfold = n_yf + 0.5;
 			qreal len = xfold*xfold + yfold*yfold;
-			kernel[n_xf][n_yf] = 0.0;
+			kernel_bevel[n_xf][n_yf] = 0.0;
+			kernel_outline[n_xf][n_yf] = 0.0;
 			if (len > radius*radius) continue;
 			len = sqrt(len);
-			// 3/pi/radius^2: normalization so that sum over all elements is (roughly) 1
-			// or precisely 1/4th, since we calculate only 1 quadrant
+			// 3/pi/radius: normalization so that sum over all elements is (roughly) 1/r
 			// (1 - len/radius)^2 : actual blur falloff function
 			qreal t = (1 - len/radius);
-			kernel[n_xf][n_yf] = 3.0 / (M_PI*radius) * (t*t);
+			kernel_bevel[n_xf][n_yf] = 3.0 / (M_PI*radius) * (t*t);
+			// outline kernel
+			if (len >= radius*outline_width + 0.5) continue;
+			if (len <= radius*outline_width - 0.5)
+			{
+				kernel_outline[n_xf][n_yf] = 1.0 / (M_PI*radius);
+			}
+			else
+			{
+				// roughly estimate coverage of that pixel
+				kernel_outline[n_xf][n_yf] = (radius*outline_width + 0.5 - len) / (M_PI*radius);
+			}
 		}
 	}
 
 	// stores the blurred "fallof" of the transparency at each point.
 	QVector<QPointF> vmap(width*height);
+	// stores how much pixel shall be darkened (outline effect)
+	QVector<qreal> darkening(width*height);
 
 	// iterate over img's pixel, finding the alpha edges
 	// we imagine the image padded into a 1px wide transparent border,
@@ -159,7 +177,7 @@ Palapeli::BevelMap Palapeli::calculateBevelMap(const QPixmap& source, int radius
 	{
 		for (int ny=-1; ny<height; ny++)
 		{
-			// for each edge, we add a blurred vector "blob" (the folding kernel) into the target vector at the right spot.
+			// for each edge, we add a blurred vector "blob" (the folding kernel_bevel) into the target vector at the right spot.
 			// the alpha values
 			int a1=0, a2=0, a3=0, a4=0;
 			if (rect.contains(nx, ny))
@@ -177,15 +195,18 @@ Palapeli::BevelMap Palapeli::calculateBevelMap(const QPixmap& source, int radius
 
 			if (dx==0 && dy==0) continue;
 
-			// iterate over the folding kernel
+			// iterate over the folding kernel_bevel
 			for (int n_xf = 0; n_xf < radius; n_xf++)
 			{
 				for (int n_yf = 0; n_yf < radius; n_yf++)
 				{
-					qreal factor = kernel[n_xf][n_yf];
-					if (factor == 0.0)
+					qreal bevelfactor = kernel_bevel[n_xf][n_yf];
+					if (bevelfactor == 0.0)
 						// only 0 will follow. break n_yf iteration and proceed with next n_xf.
 						break;
+					qreal darkenfactor = kernel_outline[n_xf][n_yf];
+					// only evaluate sqrt if necessary
+					if (darkenfactor>0) darkenfactor *= sqrt(dx*dx + dy*dy);
 					for (int quadrant=0; quadrant<4; quadrant++) {
 						// the coordinates of the point we write to
 						int n_xtick = (quadrant%2 == 0) ? nx - n_xf : nx + 1 + n_xf;
@@ -193,7 +214,8 @@ Palapeli::BevelMap Palapeli::calculateBevelMap(const QPixmap& source, int radius
 						//if (rect.contains(n_xtick, n_ytick))
 						if (n_xtick>=0 && n_ytick>=0 && n_xtick < width && n_ytick < height)
 						{
-							vmap[n_xtick + n_ytick * width] += QPointF(factor*dx, factor*dy);
+							vmap[n_xtick + n_ytick * width] += QPointF(bevelfactor*dx, bevelfactor*dy);
+							darkening[n_xtick + n_ytick * width] += darkenfactor;
 						}
 					}
 				}
@@ -227,7 +249,9 @@ Palapeli::BevelMap Palapeli::calculateBevelMap(const QPixmap& source, int radius
 			last_nonzero = idx;
 			result[idx].strength = strength;
 			result[idx].angle = int(l.angle() * 256 / 360 + 256) % 256;
-			result[idx].orig_argb = *((QRgb*)(img.scanLine(idx/width) + 4*(idx%width)));
+			QColor color = QColor::fromRgba(*((QRgb*)(img.scanLine(idx/width) + 4*(idx%width))));
+			if (darkening[idx] > 0) color = color.darker(100 + darkening[idx]*outline_darken_scale);
+			result[idx].orig_argb = color.rgba();
 		}
 	}
 	// encode last zero run if present
@@ -240,9 +264,11 @@ Palapeli::BevelMap Palapeli::calculateBevelMap(const QPixmap& source, int radius
 
 	// cleanup
 	for (int n_xf=0; n_xf < radius; n_xf ++) {
-		delete[] kernel[n_xf];
+		delete[] kernel_bevel[n_xf];
+		delete[] kernel_outline[n_xf];
 	}
-	delete[] kernel;
+	delete[] kernel_bevel;
+	delete[] kernel_outline;
 
 	return result;
 }
@@ -396,7 +422,23 @@ Palapeli::BevelMap Palapeli::mergeBevelMaps(const QList<Palapeli::PieceVisuals>&
 				}
 				else
 				{
-					result[dstidx].strength = 0;
+					int a1 = qAlpha(result[dstidx].orig_argb);
+					int a2 = qAlpha(bevelMaps[i][srcidx].orig_argb);
+					// pixel with most opacity wins
+					if (a2 > a1) result[dstidx] = bevelMaps[i][srcidx];
+					QColor color = QColor::fromRgba(result[dstidx].orig_argb);
+					// compensate premultiplication of components
+					color = color.lighter(255 * 100 / color.alpha());
+					result[dstidx].orig_argb = color.rgb();
+					
+					// reduce strength if alpha difference is low, to avoid aliasing at the edges.
+					int strength = result[dstidx].strength;
+					strength = strength * (a1-a2) / 255;
+					if (strength==0)
+						strength=1;
+					else
+						strength = (strength<0) ? -strength: strength;
+					result[dstidx].strength = strength;
 				}
 			}
 		}
