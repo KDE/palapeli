504e9252bee03d1db1f85768c7f93ae0761517ab
Ian Wadham <iandw.au@gmail.com>
2014-04-04 09:39:55 +1100

    Correct the sizing and alignment of minimal-size piece-holders.

--- src/window/pieceholder.cpp
+++ src/window/pieceholder.cpp
@@ -37,7 +37,14 @@ Palapeli::PieceHolder::PieceHolder(QWidget* parent, const QSizeF& pieceArea,
 	// Allow space for (2 * 2) pieces in minimum view initially.
 	m_scene->setPieceAreaSize(pieceArea);
 	m_scene->initializeGrid(QPointF(0.0, 0.0));
-	m_scene->setSceneRect(m_scene->piecesBoundingRect(minGrid));
+	m_scene->setMinGrid(minGrid);
+	m_scene->setSceneRect(m_scene->piecesBoundingRect());
+	/* IDW test.
+	QRectF rect = m_scene->piecesBoundingRect();
+	qreal handleWidth = qMin(rect.width(), rect.height())/25.0;
+	// Add margin for constraint_handles+spacer and setSceneRect().
+	m_scene->addMargin(handleWidth, 0.5*handleWidth);
+	*/
 	setWindowTitle(title);
 	qreal s = calculateCloseUpScale();
 	QRectF r = m_scene->sceneRect();
