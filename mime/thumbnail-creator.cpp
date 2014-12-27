a6b6188b98de315915a8a9067e3a0e98247d0a44
Montel Laurent <montel@kde.org>
2014-12-27 09:51:22 +0100

    Make it compile

--- mime/thumbnail-creator.cpp
+++ mime/thumbnail-creator.cpp
@@ -17,8 +17,8 @@
  *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 ***************************************************************************/
 
-#include <ThumbCreator>
-
+#include <KIO/ThumbCreator>
+#include <kdemacros.h>
 #include <QImage>
 #include <KTar>
 #include <KTempDir>
