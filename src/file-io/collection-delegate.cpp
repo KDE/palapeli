50fc50ed5f642449660146e88dfe00fe12ffb961
Ian Wadham <iandw.au@gmail.com>
2015-08-18 13:42:17 +1000

    Tidy indents, white space, braces and comments in Collection Delegate.
    Two files (src/file-io/collection-delegate.*) were tidied, to prepare
    for using multiple columns in the QListView of the puzzle collection.

--- src/file-io/collection-delegate.cpp
+++ src/file-io/collection-delegate.cpp
@@ -27,121 +27,153 @@
 #include <QPainter>
 #include <KLocalizedString>
 
-const QSize Palapeli::PuzzleMetadata::ThumbnailBaseSize(64, 64);
+const QSize Palapeli::PuzzleMetadata::ThumbnailBaseSize (64, 64);
 
 namespace Metrics
 {
-	const int Padding = 6;
+    const int Padding = 6;
 }
 
-Palapeli::CollectionDelegate::CollectionDelegate(QObject* parent)
-	: QStyledItemDelegate(parent)
+Palapeli::CollectionDelegate::CollectionDelegate (QObject* parent)
+        : QStyledItemDelegate(parent)
 {
-	QAbstractItemView* view = qobject_cast<QAbstractItemView*>(parent);
-	if (view)
-		view->setItemDelegate(this);
+    QAbstractItemView* view = qobject_cast<QAbstractItemView*>(parent);
+    if (view)
+        view->setItemDelegate(this);
 }
 
 QRect Palapeli::CollectionDelegate::thumbnailRect(const QRect& baseRect) const
 {
-	QRect thumbnailBaseRect(QPoint(Metrics::Padding + baseRect.left(), 0), Palapeli::PuzzleMetadata::ThumbnailBaseSize);
-	thumbnailBaseRect.moveCenter(QPoint(thumbnailBaseRect.center().x(), baseRect.center().y()));
-	if (QApplication::isRightToLeft())
-		thumbnailBaseRect.moveRight(baseRect.right() - Metrics::Padding);
-	return thumbnailBaseRect;
+    QRect thumbnailBaseRect (QPoint(Metrics::Padding + baseRect.left(), 0),
+                             Palapeli::PuzzleMetadata::ThumbnailBaseSize);
+    thumbnailBaseRect.moveCenter (QPoint(thumbnailBaseRect.center().x(),
+                                  baseRect.center().y()));
+    if (QApplication::isRightToLeft()) {
+        thumbnailBaseRect.moveRight (baseRect.right() - Metrics::Padding);
+    }
+    return thumbnailBaseRect;
 }
 
-void Palapeli::CollectionDelegate::paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const
+void Palapeli::CollectionDelegate::paint (
+                                        QPainter* painter,
+                                        const QStyleOptionViewItem& option,
+                                        const QModelIndex& index) const
 {
-	paintItem(painter, option, index);
+    paintItem (painter, option, index);
 }
 
-void Palapeli::CollectionDelegate::paintItem(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const
+void Palapeli::CollectionDelegate::paintItem (
+                                        QPainter* painter,
+                                        const QStyleOptionViewItem& option,
+                                        const QModelIndex& index) const
 {
-	const bool rtl = option.direction == Qt::RightToLeft;
-	QRect baseRect = option.rect;
-	//draw background
-	QApplication::style()->drawPrimitive(QStyle::PE_PanelItemViewItem, &option, painter, 0);
-	//draw thumbnail
-	QRect thumbnailBaseRect = this->thumbnailRect(baseRect);
-	const QPixmap thumbnail = index.data(Palapeli::Collection::ThumbnailRole).value<QPixmap>();
-	QRect thumbnailRect(thumbnailBaseRect.topLeft(), thumbnail.size());
-	thumbnailRect.translate( //center inside thumbnailBaseRect
-		(thumbnailBaseRect.width() - thumbnailRect.width()) / 2,
-		(thumbnailBaseRect.height() - thumbnailRect.height()) / 2
-	);
-	painter->drawPixmap(thumbnailRect.topLeft(), thumbnail);
-	//find metrics: text
-	QStringList texts; QList<QFont> fonts;
-	{
-		QString name = index.data(Palapeli::Collection::NameRole).toString();
-		const int pieceCount = index.data(Palapeli::Collection::PieceCountRole).toInt();
-		if (name.isEmpty())
-			name = i18n("[No name]");
-		if (pieceCount > 0)
-			name = ki18ncp("Puzzle description, %2 = name string, %1 = piece count", "%2 (%1 piece)", "%2 (%1 pieces)")
-		       .subs(pieceCount).subs(name).toString();
-		texts << name;
-		QFont theFont(painter->font()); theFont.setBold(true); fonts << theFont;
-	}{
-		QString comment = index.data(Palapeli::Collection::CommentRole).toString();
-		if (!comment.isEmpty())
-		{
-			texts << comment;
-			fonts << painter->font();
-		}
-	}{
-		QString author = index.data(Palapeli::Collection::AuthorRole).toString();
-		if (!author.isEmpty())
-		{
-			const QString authorString = ki18nc("Author attribution, e.g. \"by Jack\"", "by %1").subs(author).toString();
-			texts << authorString;
-			QFont theFont(painter->font()); theFont.setItalic(true); fonts << theFont;
-		}
-	}
-	QList<QRect> textRects; int totalTextHeight = 0;
-	for (int i = 0; i < texts.count(); ++i)
-	{
-		QFontMetrics fm(fonts[i]);
-		textRects << fm.boundingRect(texts[i]);
-		textRects[i].setHeight(qMax(textRects[i].height(), fm.lineSpacing()));
-		totalTextHeight += textRects[i].height();
-	}
-	QRect textBaseRect(baseRect);
-	if (rtl)
-	{
-		textBaseRect.moveRight(thumbnailBaseRect.left() - Metrics::Padding);
-		textBaseRect.adjust(Metrics::Padding, Metrics::Padding, 0, -Metrics::Padding);
-	}
-	else
-	{
-		textBaseRect.moveLeft(thumbnailBaseRect.right() + Metrics::Padding);
-		textBaseRect.adjust(0, Metrics::Padding, -Metrics::Padding, -Metrics::Padding);
-	}
-	textBaseRect.setHeight(totalTextHeight);
-	textBaseRect.moveTop(baseRect.top() + (baseRect.height() - textBaseRect.height()) / 2);
-	//draw texts
-	QRect currentTextRect(textBaseRect);
-	painter->save();
-	for (int i = 0; i < texts.count(); ++i)
-	{
-		painter->setFont(fonts[i]);
-		const QRect& textRect = textRects[i];
-		currentTextRect.setHeight(textRect.height());
-		painter->drawText(currentTextRect, Qt::AlignLeft | Qt::AlignVCenter, texts[i]);
-		currentTextRect.moveTop(currentTextRect.bottom());
-	}
-	painter->restore();
+    const bool rtl = option.direction == Qt::RightToLeft;
+    QRect baseRect = option.rect;
+    // Draw background.
+    QApplication::style()->drawPrimitive (QStyle::PE_PanelItemViewItem,
+                                          &option, painter, 0);
+    // Draw thumbnail.
+    QRect thumbnailBaseRect = this->thumbnailRect (baseRect);
+    const QPixmap thumbnail = index.data (Palapeli::Collection::ThumbnailRole)
+                                          .value<QPixmap>();
+    QRect thumbnailRect (thumbnailBaseRect.topLeft(), thumbnail.size());
+    thumbnailRect.translate (		// Center inside thumbnailBaseRect.
+                (thumbnailBaseRect.width()  - thumbnailRect.width()) / 2,
+                (thumbnailBaseRect.height() - thumbnailRect.height()) / 2);
+    painter->drawPixmap (thumbnailRect.topLeft(), thumbnail);
+    // Find metrics for text.
+    QStringList texts; QList<QFont> fonts;
+    {
+        QString name = index.data(Palapeli::Collection::NameRole).toString();
+        const int pieceCount = index.data (Palapeli::Collection::PieceCountRole)
+                                           .toInt();
+        if (name.isEmpty()) {
+            name = i18n("[No name]");
+        }
+        if (pieceCount > 0) {
+            name = ki18ncp (
+                    "Puzzle description, %2 = name string, %1 = piece count",
+                    "%2 (%1 piece)",
+                    "%2 (%1 pieces)")
+                    .subs(pieceCount).subs(name).toString();
+        }
+        texts << name;
+        QFont theFont (painter->font());
+        theFont.setBold(true);
+        fonts << theFont;
+    }{
+        QString comment = index.data (Palapeli::Collection::CommentRole)
+                                      .toString();
+        if (!comment.isEmpty()) {
+            texts << comment;
+            fonts << painter->font();
+        }
+    }{
+        QString author = index.data (Palapeli::Collection::AuthorRole)
+                                     .toString();
+        if (!author.isEmpty()) {
+            const QString authorString = ki18nc (
+                        "Author attribution, e.g. \"by Jack\"",
+                        "by %1")
+                        .subs(author).toString();
+            texts << authorString;
+            QFont theFont (painter->font());
+            theFont.setItalic(true);
+            fonts << theFont;
+        }
+    }
+    QList<QRect> textRects; int totalTextHeight = 0;
+    for (int i = 0; i < texts.count(); ++i) {
+        QFontMetrics fm(fonts[i]);
+        textRects << fm.boundingRect(texts[i]);
+        textRects[i].setHeight(qMax(textRects[i].height(), fm.lineSpacing()));
+        totalTextHeight += textRects[i].height();
+    }
+    QRect textBaseRect(baseRect);
+    if (rtl) {
+        textBaseRect.moveRight (thumbnailBaseRect.left() - Metrics::Padding);
+        textBaseRect.adjust (Metrics::Padding, Metrics::Padding,
+                             0, -Metrics::Padding);
+    }
+    else {
+        textBaseRect.moveLeft (thumbnailBaseRect.right() + Metrics::Padding);
+        textBaseRect.adjust (0, Metrics::Padding,
+                             -Metrics::Padding, -Metrics::Padding);
+    }
+    textBaseRect.setHeight (totalTextHeight);
+    textBaseRect.moveTop (baseRect.top() +
+                          (baseRect.height() - textBaseRect.height()) / 2);
+
+    // Draw texts.
+    QRect currentTextRect (textBaseRect);
+    painter->save();
+    for (int i = 0; i < texts.count(); ++i) {
+        painter->setFont(fonts[i]);
+        const QRect& textRect = textRects[i];
+        currentTextRect.setHeight(textRect.height());
+        painter->drawText (currentTextRect,
+                           Qt::AlignLeft | Qt::AlignVCenter,
+                           texts[i]);
+        currentTextRect.moveTop (currentTextRect.bottom());
+    }
+    painter->restore();
 }
 
-void Palapeli::CollectionDelegate::paintHeader(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const
+void Palapeli::CollectionDelegate::paintHeader (
+                                        QPainter* painter,
+                                        const QStyleOptionViewItem& option,
+                                        const QModelIndex& index) const
 {
-	QStyledItemDelegate::paint(painter, option, index);
+    QStyledItemDelegate::paint (painter, option, index);
 }
 
-QSize Palapeli::CollectionDelegate::sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const
+QSize Palapeli::CollectionDelegate::sizeHint (
+                                        const QStyleOptionViewItem& option,
+                                        const QModelIndex& index) const
 {
-	Q_UNUSED(option) Q_UNUSED(index)
-	//TODO: take text size into account
-	return QSize(400, Palapeli::PuzzleMetadata::ThumbnailBaseSize.height() + 2 * Metrics::Padding);
+    Q_UNUSED(option) Q_UNUSED(index)
+    //TODO: take text size into account
+    return QSize (400,
+                  Palapeli::PuzzleMetadata::ThumbnailBaseSize.height() +
+                  2 * Metrics::Padding);
 }
--- src/file-io/collection-delegate.h
+++ src/file-io/collection-delegate.h
@@ -23,17 +23,25 @@
 
 namespace Palapeli
 {
-	class CollectionDelegate : public QStyledItemDelegate
-	{
-		public:
-			CollectionDelegate(QObject* parent = 0);
-			virtual void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const;
-			virtual QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const;
-			QRect thumbnailRect(const QRect& baseRect) const;
-		private:
-			void paintItem(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const;
-			void paintHeader(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const;
-	};
+    class CollectionDelegate : public QStyledItemDelegate
+    {
+    public:
+        CollectionDelegate     (QObject* parent = 0);
+
+        virtual void paint     (QPainter* painter,
+                                const QStyleOptionViewItem& option,
+                                const QModelIndex& index) const;
+        virtual QSize sizeHint (const QStyleOptionViewItem& option,
+                                const QModelIndex& index) const;
+
+        QRect thumbnailRect    (const QRect& baseRect) const;
+
+    private:
+        void paintItem (QPainter* painter, const QStyleOptionViewItem& option,
+                        const QModelIndex& index) const;
+        void paintHeader (QPainter* painter, const QStyleOptionViewItem& option,
+                          const QModelIndex& index) const;
+    };
 }
 
 #endif // PALAPELI_COLLECTIONDELEGATE_H
