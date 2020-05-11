4f6145237d46640fb09cb7c03ce918b73f2aada0
Matej Mrenica <matejm98mthw@gmail.com>
2020-05-11 20:16:56 +0300

    Fix blurry icons by adding UseHighDpiPixmaps flag
    Summary: When using fractional scaling, icons on the top are blurred. This patch fixes the issue.
    
    Test Plan: No side effects.
    
    Reviewers: #kde_games, yurchor
    
    Reviewed By: yurchor
    
    Subscribers: yurchor, kde-games-devel
    
    Differential Revision: https://phabricator.kde.org/D29659

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,8 @@
 
 int main(int argc, char** argv)
 {
+    // Fixes blurry icons with fractional scaling
+    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication app(argc, argv);
 
     KLocalizedString::setApplicationDomain("palapeli");
